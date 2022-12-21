4737bc35e48a8eefd359561f0f17f6dfe2528bc3
Gabor Greif <gabor@dfinity.org>
2022-12-21 14:29:19 +0100

    Update provision-linux.sh

--- .github/workflows/provision-linux.sh
+++ .github/workflows/provision-linux.sh
@@ -13,12 +13,12 @@ rm install-node.sh
 
 # Install DFINITY SDK.
 wget --output-document install-dfx.sh "https://internetcomputer.org/install.sh"
-DFX_VERSION=${DFX_VERSION:=0.12.0} bash install-dfx.sh < <(yes Y)
+DFX_VERSION=${DFX_VERSION:=0.12.1} bash install-dfx.sh < <(yes Y)
 rm install-dfx.sh
 dfx cache install
 
 # Install ic-repl
-version=0.1.2
+version=0.3.10
 curl --location --output ic-repl "https://github.com/chenyan2002/ic-repl/releases/download/$version/ic-repl-linux64"
 mv ./ic-repl /usr/local/bin/ic-repl
 chmod a+x /usr/local/bin/ic-repl
@@ -39,7 +39,7 @@ rm "v${matchers_version}.tar.gz"
 mv "$(dfx cache show)/motoko-matchers-${matchers_version}" "$(dfx cache show)/motoko-matchers"
 
 # Install wasmtime
-wasmtime_version=0.33.1
+wasmtime_version=3.0.1
 curl -fsSLO "https://github.com/bytecodealliance/wasmtime/releases/download/v${wasmtime_version}/wasmtime-v${wasmtime_version}-x86_64-linux.tar.xz" 
 mkdir -p "${HOME}/bin"
 tar -xf "wasmtime-v${wasmtime_version}-x86_64-linux.tar.xz" --directory "${HOME}/bin/"
