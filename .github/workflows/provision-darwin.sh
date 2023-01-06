d96cf2e72aac2e10176825fe700bf9fb66a280a4
Gabor Greif <gabor@dfinity.org>
2023-01-06 16:42:53 +0100

    more update and simplify

--- .github/workflows/provision-darwin.sh
+++ .github/workflows/provision-darwin.sh
@@ -10,15 +10,9 @@ curl --location --output install-brew.sh "https://raw.githubusercontent.com/Home
 bash install-brew.sh
 rm install-brew.sh
 
-# Install Node.
-version=14.15.4
-curl --location --output node.pkg "https://nodejs.org/dist/v$version/node-v$version.pkg"
-sudo installer -pkg node.pkg -store -target /
-rm node.pkg
-
 # Install DFINITY SDK.
 curl --location --output install-dfx.sh "https://internetcomputer.org/install.sh"
-DFX_VERSION=${DFX_VERSION:=0.12.0} bash install-dfx.sh < <(yes Y)
+DFX_VERSION=${DFX_VERSION:=0.12.1} bash install-dfx.sh < <(yes Y)
 rm install-dfx.sh
 dfx cache install
 
